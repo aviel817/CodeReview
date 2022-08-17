cba9227405d857d31b6c6f3992c30055328c27e4
segevmn <Mn94segm>
2022-08-17 21:26:41 +0300

    register update

--- register.js
+++ register.js
@@ -1,1 +1,25 @@
-//TEST
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    totalPoints: Number,
+    projects: [Number],
+    permission : Number,
+    email: String
+}, {
+    collection: 'Users'
+});
+
+const User = mongoose.model('Users', UserSchema);
+
+router.get('/', function (req, res) {
+	return res.render('register.html');
+});
+router.post('/', function(req, res) {
+	console.log(req.body);
+    
+
+});
