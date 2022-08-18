2af64e09bfe364968937c1ccbe7d886cf05c4264
segevmn <Mn94segm>
2022-08-18 12:14:39 +0300

    changes to register

--- register.js
+++ register.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const path = require('path');
+const argon2 = require('argon2');
 var bodyParser = require('body-parser')
 
 // create application/x-www-form-urlencoded parser
@@ -25,6 +26,36 @@ router.get('/', function (req, res) {
 });
 router.post('/', urlencodedParser, async(req, res) =>  {
 	console.log(req.body);
+    const user = await User.findOne({
+        username: req.body.username
+    });
+    if(user){
+        res.redirect('/register');
+    }
+    else{
+        const userEmail = await User.findOne({
+            email: req.body.email            
+        });
+        if(userEmail){
+            res.redirect('/register');
+        }
+        else {
+            try {
+                var hash = await argon2.hash("password");
+              } catch (err) {
+                console.log("ERROR " + err);
+              }   
+            new User({
+                username: req.body.username,
+                password: hash,
+                totalPoints: 0,
+                projects: [0, 1],
+                permission: 0,
+                email: req.body.email
+            }).save()
+            res.redirect('/login');
+        }
+    }
     
 });
 
